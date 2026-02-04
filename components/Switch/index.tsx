6acd2a56affec2c3fb8e06c75127027f9e1392da
usamadoch <usamadoch@gmail.com>
2026-02-04 14:23:54 +0500

    Update client dependencies and components

--- components/Switch/index.tsx
+++ components/Switch/index.tsx
@@ -1,3 +1,8 @@
+
+
+
+
+
 import { Switch as SwitchReact } from "@headlessui/react";
 import { twMerge } from "tailwind-merge";
 
@@ -12,17 +17,15 @@ const Switch = ({ className, value, setValue }: SwitchProps) => (
         <SwitchReact
             checked={value}
             onChange={setValue}
-            className={`relative inline-flex items-center w-10 h-6 pl-0.75 cursor-pointer rounded-sm border border-n-1 transition-colors outline-none dark:border-white ${
-                value ? "bg-purple-1" : "bg-purple-3"
-            }`}
+            className={`relative inline-flex items-center w-10 h-6 pl-0.75 cursor-pointer rounded-sm border border-n-1 transition-colors outline-none dark:border-white ${value ? "bg-purple-1" : "bg-purple-3"
+                }`}
         >
             <span
                 aria-hidden="true"
                 className={twMerge(
-                    `pointer-events-none inline-block w-4 h-4 transition-all ${
-                        value
-                            ? "translate-x-4 bg-n-1"
-                            : "translate-x-0 bg-purple-1"
+                    `pointer-events-none inline-block w-4 h-4 transition-all ${value
+                        ? "translate-x-4 bg-n-1"
+                        : "translate-x-0 bg-purple-1"
                     }`
                 )}
             />
